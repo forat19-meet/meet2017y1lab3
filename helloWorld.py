fd673fe0e66faa49f67574a99c23d6662f29ee62
forat19-meet <forat19@meet.mit.edu>
2017-07-25 12:28:20 +0300

    Stopping here

---
+++ helloWorld.py
@@ -0,0 +1,49 @@
+>>> print("hello world")
+hello world
+>>> print('hello world')
+hello world
+>>> print("hello gilad")
+hello gilad
+>>> # print("Hello World!")
+>>> print("Γειά σου Κόσμε")
+Γειά σου Κόσμε
+>>> print("Olá Mundo")
+Olá Mundo
+>>> print("Ahoj světe")
+Ahoj světe
+>>> print("안녕 세상")
+안녕 세상
+>>> print("你好，世界")
+你好，世界
+>>> print("नमस्ते दुनिया")
+नमस्ते दुनिया
+>>> print("ुSalamu, Dunia")
+ुSalamu, Dunia
+>>> print("Hola Mundo")
+Hola Mundo
+>>> print("ສະ​ບາຍ​ດີ​ຊາວ​ໂລກ")
+ສະ​ບາຍ​ດີ​ຊາວ​ໂລກ
+>>> print("Witaj świecie")
+Witaj świecie
+print("Γειά σου Κόσμε is how you say hello world in (greek).")
+>>> print("ola mondo is how you say hello world in (portuguese).")
+ola mondo is how you say hello world in (portuguese).
+>>> print("Γειά σου Κόσμε is how you say hello world in (greek).")
+Γειά σου Κόσμε is how you say hello world in (greek).
+>>> print("Ahoj světe is how you say hello world in (czech).")
+Ahoj světe is how you say hello world in (czech).
+>>> print("안녕 세상 is how you say hello world in (korean).")
+안녕 세상 is how you say hello world in (korean).
+>>> print("你好，世界 is how you say hello world in (chinese).")
+你好，世界 is how you say hello world in (chinese).
+>>> print("नमस्ते दुनिया is how you say hello world in (hindi).")
+नमस्ते दुनिया is how you say hello world in (hindi).
+>>> print("ुSalamu, Dunia is how you say hello world in (swahili).")
+ुSalamu, Dunia is how you say hello world in (swahili).
+>>> print("ुhola mundo is how you say hello world in (spanish).")
+ुhola mundo is how you say hello world in (spanish).
+>>> print("ुWitaj świecie is how you say hello world in (polan).")
+ुWitaj świecie is how you say hello world in (polan).
+>>> print("ुສະ​ບາຍ​ດີ​ຊາວ​ໂລກ is how you say hello world in (lau).")
+ुສະ​ບາຍ​ດີ​ຊາວ​ໂລກ is how you say hello world in (lau).
+>>> 
---
+++ lab 2.py
@@ -0,0 +1,47 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> "Don't" + "forget" + "to" + "conserve" + "water"
+"Don'tforgettoconservewater"
+>>> 'Don't' + ' forget' + 'to' + ' conserve ' +
+
+'water'
+SyntaxError: invalid syntax
+>>> "the"*3
+'thethethe'
+>>> 'the' + 3
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    'the' + 3
+TypeError: Can't convert 'int' object to str implicitly
+>>> "the"*3 + "beautiful" + "Earth"
+'thethethebeautifulEarth'
+>>> 2*"True"
+'TrueTrue'
+>>> #that is Q1 lab2
+>>> #and now Q2 lab2
+>>> 
+>>> a = 'save'
+
+>>> b = 'the'
+
+>>> c = 'planet'
+SyntaxError: invalid syntax
+>>> a = 'save'
+>>> b = 'the'
+>>> a = 'save '
+>>> b = 'the '
+>>> c = 'planet '
+>>> a+b+c
+'save the planet '
+>>> #Q3
+>>> a = 4
+>>> b = "panda bears"
+>>> b= "panda bears "
+>>> str(a) + b
+'4panda bears '
+>>> #Q4
+>>> a= "forat is the best"
+>>> len(a)
+17
+>>> 
