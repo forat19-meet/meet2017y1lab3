61f85c87f28ade38e62ddd0f2a2ba852ad44db8e
forat19-meet <forat19@meet.mit.edu>
2017-07-25 18:37:16 +0300

    Stopping here

---
+++ age.py
@@ -0,0 +1,23 @@
+print("How old are you?")
+x = input()
+print("How old are you?")
+z = input()
+y = x+z
+print (y)
+
+
+
+
+
+
+
+
+
+
+# This code is incorrect! Fix it and make it better!
+student = input("What is your name?")
+print(student + "'can't wait to start writing harder programs")
+instructor = input("Who is your favorite instructor?")
+print("Your favorite instructor is" + instructor + "and she is aoswme")
+team = input("Which team are you on?")
+print(team + " is the best team in all of MEET")
